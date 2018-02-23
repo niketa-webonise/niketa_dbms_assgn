ca256a68ec910d573a7669b00c1106b8c21f7f0c
niketaj <niketa.jain@weboniselab.com>
2018-02-23 16:09:10 +0530

    added

--- dbmsassgn/dbms_assn.sql
+++ dbmsassgn/dbms_assn.sql
@@ -1,31 +1,28 @@
-/*create table user
-(user_id int(10),
- name  varchar(20) not null,
- email varchar(20) not null ,
- pwd varchar(20) not null,
- type text not null,
- primary key(user_id),
- unique(email));
+/*create table type
+(id int(10),
+ name varchar(50),
+primary key(id));
 
-create trigger trigger_name_check before insert on user
-for each row
-begin
-if name like '^[a-zA-Z]+$' then
-   insert into user(name)values('&name');
-else
-   set message='enter valid data';
-end if;
-end;
+create table users
+(id int(10),
+ name text,
+ pwd varchar(50),
+ email varchar(80),
+ phone int(10),
+ primary key(id),
+ type_id int(10) REFERENCES type(id));
 
-alter table user
-modify column email varchar(80);
+create table types
+(id int(10),
+ name varchar(50),
+ primary key(id));*/
 
-insert into user(user_id,name,email,pwd,type)values(1,'Rakesh kumar','rakeshkumar@gmail.com',12345,'buyer');
+create table payments
+(id int(30),
+ status varchar(40),
+primary key(id));
 
-insert into user(user_id,name,email,pwd,type)values(2,'Rohit Kumar','rohitkumar@gmail.com','abcdef','invertory manager');*/
 
-insert into user(user_id,name,email,pwd,type)values(3,'Rohan Jain','rohit.jain@outlook.com',445566,'buyer');
 
-insert into user(user_id,name,email,pwd,type)values(4,'Monika Sharma','monikasharma@gmail.com','monika','invertory manager');
 
-insert into user(user_id,name,email,pwd,type)values(5,'Swati Gupta','swatigupta12@gmail.com','swatigupta','buyer');
+
