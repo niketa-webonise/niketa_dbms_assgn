466fdc39fad0b766bbc52770ed22ded9d92fcb0d
niketaj <niketa.jain@weboniselab.com>
2018-02-23 20:12:39 +0530

    added

--- dbmsassgn/dbms_assn.sql
+++ dbmsassgn/dbms_assn.sql
@@ -129,7 +129,7 @@ insert into discounts
 (id,type)values(1,0.8);
 
 insert into discounts
-(id,type)values(2,0.50);*/
+(id,type)values(2,0.50);
 
 insert into discounts
 (id,type)values(3,0.30);
@@ -137,12 +137,92 @@ insert into discounts
 insert into discounts
 (id,type)values(4,0.40);
 
+insert into payments(id,status)values(1,'complete');
+insert into payments(id,status)values(2,'in progress');
+insert into payments(id,status)values(3,'complete');
+insert into payments(id,status)values(4,'complete');
+
+insert into colors(id,name)values(1,'RED');
+insert into colors(id,name)values(2,'BLUE');
+insert into colors(id,name)values(3,'RED');
+insert into colors(id,name)values(4,'ORANGE');
+
+insert into suppliers(id,name)values(1,'Rakesh');
+insert into suppliers(id,name)values(2,'Rahu');
+insert into suppliers(id,name)values(3,'Rahul');
+insert into suppliers(id,name)values(4,'Rajni');
+insert into suppliers(id,name)values(5,'Anmol');
+
+insert into cart(id,phone,email,user_id,color_id,product_id)
+values(1,123456789,'j@gmail.com',1,1,1);
+
+insert into cart(id,phone,email,user_id,color_id,product_id)
+values(2,123456744,'jaw@gmail.com',2,1,1);
+
+insert into cart(id,phone,email,user_id,color_id,product_id)
+values(3,123456789,'jrg@gmail.com',1,2,2);
+
+insert into cart(id,phone,email,user_id,color_id,product_id)
+values(4,123456789,'jth@gmail.com',3,2,1);
+
+insert into cart(id,phone,email,user_id,color_id,product_id)
+values(5,123456789,'jqw@gmail.com',1,3,2);
+
+insert into orders(id,order_number,payment_id,price,order_date,user_id,quantity)
+values(1,1234,1,5000,'2017-09-1',2,30);
+
+insert into orders(id,order_number,payment_id,price,order_date,user_id,quantity)
+values(2,12122,1,15000,'2015-02-15',2,4);
+
+insert into orders(id,order_number,payment_id,price,order_date,user_id,quantity)
+values(3,111,2,6000,'2018-04-18',1,4);
+
+insert into orders(id,order_number,payment_id,price,order_date,user_id,quantity)
+values(4,1234,4,5000,'2018-01-21',4,8);
+
+insert into orders(id,order_number,payment_id,price,order_date,user_id,quantity)
+values(5,1234,1,5000,'2013-05-16',2,12);
+
+alter table products
+drop column amount;
+
+insert into products(id,supplier_id,price,discount_id,color_id,quantity)
+values(1,1,12000,1,1,20);
+
+insert into products(id,supplier_id,price,discount_id,color_id,quantity)
+values(2,2,12000,1,2,2);
+
+insert into products(id,supplier_id,price,discount_id,color_id,quantity)
+values(3,1,12000,1,3,3);
+
+insert into products(id,supplier_id,price,discount_id,color_id,quantity)
+values(4,3,12000,1,1,6);
+
+insert into products(id,supplier_id,price,discount_id,color_id,quantity)
+values(5,2,12000,1,4,40);
 
 
 
+alter table order_details
+drop column price,drop column quantity,drop column total;
+*/
+
+insert into order_details(order_id,product_id,discount_id)
+values(1,2,1);
+
+insert into order_details(order_id,product_id,discount_id)
+values(2,2,1);
+
+insert into order_details(order_id,product_id,discount_id)
+values(2,1,1);
 
 
+insert into order_details(order_id,product_id,discount_id)
+values(3,2,1);
 
+
+insert into order_details(order_id,product_id,discount_id)
+values(1,4,1);
 
 
 
